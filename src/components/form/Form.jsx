e985caccd3aacea2547eb627c7acfbf435ff48ba
KamilD90 <117952413+KamilD90@users.noreply.github.com>
2023-06-18 20:08:09 +0200

    works

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,58 +1,56 @@
 import { Component } from 'react';
-// import { nanoid } from 'https://cdn.jsdelivr.net/npm/nanoid/nanoid.js'
 
 class Form extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      contacts: [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ],
-      filter: '',
       name: '',
       number: '',
     };
   }
 
   handleChange = event => {
-    this.setState({ name: event.target.value });
-    console.log(this.state.name);
+    const { value, name } = event.target;
+    this.setState({ [name]: value });
   };
 
   handleSubmit = event => {
     event.preventDefault();
-    // const form = event.currentTarget;
-    // const name = form.elements.name.value;
-    // const number = form.elements.number.value;
+    const { name, number } = this.state;
+    console.log(`name: ${name}, Number: ${number}`);
     this.props.onSubmit({ ...this.state });
+    this.reset();
   };
 
-  //zmiana stanu powoduje re-renderowanie
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
   render() {
-    const name = this.state.name;
+    const { name, number } = this.state;
 
     return (
       <form onSubmit={this.handleSubmit}>
-        <label for="name">Imię:</label>
+        <label htmlFor="name">Imię:</label>
         <input
           id="name"
           value={name}
           onChange={this.handleChange}
           type="text"
           name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore        d'Artagnan"
+          pattern="^[a-zA-Z]+([-'\\s][a-zA-Z]+)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
         />
-        <label for="number">Numer telefonu:</label>
+        <label htmlFor="number">Numer telefonu:</label>
         <input
           id="number"
+          value={number}
+          onChange={this.handleChange}
           type="tel"
           name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          pattern="^[a-zA-Z]+([-'\\s][a-zA-Z]+)*$"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
         />
